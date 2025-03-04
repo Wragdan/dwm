890fc385b144732026534c37fe5efcd235bec248
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-03-03 21:08:18 -0600

    feat: apply dwmc patch and start moving keybinds to sxhkd

--- config.h
+++ config.h
@@ -122,8 +122,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_k,          focusstack,         {.i = -1 } },
 	{ MODKEY,                       XK_o,          incnmaster,         {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,          incnmaster,         {.i = -1 } },
-	{ MODKEY,                       XK_h,          setmfact,           {.f = -0.05} },
-	{ MODKEY,                       XK_l,          setmfact,           {.f = +0.05} },
+	//{ MODKEY,                       XK_h,          setmfact,           {.f = -0.05} },
+	//{ MODKEY,                       XK_l,          setmfact,           {.f = +0.05} },
   { MODKEY,                       XK_Return,     spawn,              {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Return,     zoom,               {0} },
 	{ MODKEY,                       XK_Tab,        view,               {0} },
@@ -170,3 +170,73 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+void
+setlayoutex(const Arg *arg)
+{
+	setlayout(&((Arg) { .v = &layouts[arg->i] }));
+}
+
+void
+viewex(const Arg *arg)
+{
+	view(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+viewall(const Arg *arg)
+{
+	view(&((Arg){.ui = ~0}));
+}
+
+void
+toggleviewex(const Arg *arg)
+{
+	toggleview(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+tagex(const Arg *arg)
+{
+	tag(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+toggletagex(const Arg *arg)
+{
+	toggletag(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+tagall(const Arg *arg)
+{
+	tag(&((Arg){.ui = ~0}));
+}
+
+/* signal definitions */
+/* signum must be greater than 0 */
+/* trigger signals using `xsetroot -name "fsignal:<signame> [<type> <value>]"` */
+static Signal signals[] = {
+	/* signum           function */
+	{ "focusstack",     focusstack },
+	{ "setmfact",       setmfact },
+	{ "togglebar",      togglebar },
+	{ "incnmaster",     incnmaster },
+	{ "togglefloating", togglefloating },
+	{ "focusmon",       focusmon },
+	{ "tagmon",         tagmon },
+	{ "zoom",           zoom },
+	{ "view",           view },
+	{ "viewall",        viewall },
+	{ "viewex",         viewex },
+	{ "toggleview",     view },
+	{ "toggleviewex",   toggleviewex },
+	{ "tag",            tag },
+	{ "tagall",         tagall },
+	{ "tagex",          tagex },
+	{ "toggletag",      tag },
+	{ "toggletagex",    toggletagex },
+	{ "killclient",     killclient },
+	{ "quit",           quit },
+	{ "setlayout",      setlayout },
+	{ "setlayoutex",    setlayoutex },
+};
